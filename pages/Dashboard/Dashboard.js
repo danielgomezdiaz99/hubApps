7a1d8fad42a6d4604956a631af4114a547820333
Daniel Gómez <dgd380@gmail.com>
2024-02-19 06:50:06 +0100

    Guardar Usuario 2

--- pages/Dashboard/Dashboard.js
+++ pages/Dashboard/Dashboard.js
@@ -1,14 +1,14 @@
 import { getData } from "../../global/state/globalState";
-import { getUser } from "../../global/state/globalState";
+import { getUserData } from "../../global/state/globalState";
 
 import { getInfo,initControler } from "../../utils";
 import "./Dashboard.css";
 import imagen from '../../img/pokeball.png';
 import hanman from '../../img/hanman.png';
-const userState = getUser().name;
-console.log(userState)
+
+
 const template = () => `
-<h2 class="welcome">Bienvenido ${userState}</h2>
+<h2 class="welcome">Bienvenido <spam id = "user"></spam></h2>
 <div class="menu-container">
   <div class="menu1">
       <h2>Poke-Api</h2>
@@ -38,7 +38,11 @@ export const printTemplateDashboard = () => {
   document.querySelector("main").innerHTML = template();
 
   document.querySelector("nav").style.display = "flex";
-
+  const userData = getUserData();
+  console.log(userData);
+  let userSite=document.getElementById("user")
+  console.log(userSite)
+  userSite.textContent=userData.name
   addEventListeners();
 
   getInfo();
